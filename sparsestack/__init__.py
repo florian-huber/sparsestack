b609eb42e9e311b4f4c02717467e0d2ac8fd8a82
florian-huber <florian.huber@hs-duesseldorf.de>
2022-08-24 12:03:49 +0200

    fix init

--- sparsestack/__init__.py
+++ sparsestack/__init__.py
@@ -2,9 +2,6 @@ from .__version__ import __version__
 from .StackedSparseArray import StackedSparseArray
 
 
-_init_logger()
-
-
 __author__ = "Florian Huber"
 __email__ = 'florian.hubern@hs-duesseldorf.de'
 __all__ = [
