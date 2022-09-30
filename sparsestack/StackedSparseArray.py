106e5469afd0a75204833f968a2dd344d92d21da
florian-huber <florian.huber@hs-duesseldorf.de>
2022-09-30 17:51:05 +0200

    fix code example

--- sparsestack/StackedSparseArray.py
+++ sparsestack/StackedSparseArray.py
@@ -27,26 +27,26 @@ class StackedSparseArray:
         import numpy as np
         from sparsestack import StackedSparseArray
 
+        # Create some fake data
         scores1 = np.random.random((12, 10))
+        scores1[scores1 < 0.9] = 0  # make "sparse"
         scores2 = np.random.random((12, 10))
-
-        scores_array = StackedSparseArray(12, 10)
-        scores_array.add_dense_matrix(scores1, "scores_1")
-        scores_array = scores_array.filter_by_range("scores_1", low=0.5)
+        scores2[scores2 < 0.75] = 0  # make "sparse"
+        sparsestack = StackedSparseArray(12, 10)
+        sparsestack.add_dense_matrix(scores1, "scores_1")
 
         # Add second scores and filter
-        scores_array.add_dense_matrix(scores2, "scores_2")
-        scores_array = scores_array.filter_by_range("scores_2", low=0.1, high=0.4)
+        sparsestack.add_dense_matrix(scores2, "scores_2", join_type="left")
 
         # Scores can be accessed using (limited) slicing capabilities
-        scores_array[3, 4]  # => scores_1 and scores_2 at position row=3, col=4
-        scores_array[3, :]  # => tuple with row, col, scores for all entries in row=3
-        scores_array[:, 2]  # => tuple with row, col, scores for all entries in col=2
-        scores_array[3, :, 0]  # => tuple with row, col, scores_1 for all entries in row=3
-        scores_array[3, :, "scores_1"]  # => same as the one before
+        sparsestack[3, 4]  # => scores_1 and scores_2 at position row=3, col=4
+        sparsestack[3, :]  # => tuple with row, col, scores for all entries in row=3
+        sparsestack[:, 2]  # => tuple with row, col, scores for all entries in col=2
+        sparsestack[3, :, 0]  # => tuple with row, col, scores_1 for all entries in row=3
+        sparsestack[3, :, "scores_1"]  # => same as the one before
 
         # Scores can also be converted to a dense numpy array:
-        scores2_after_filtering = scores_array.to_array("scores_2")
+        scores2_after_merge = sparsestack.to_array("scores_2")
 
     """
     def __init__(self, n_row, n_col):
