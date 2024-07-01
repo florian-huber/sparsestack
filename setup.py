6c912c0f5a5bc623acd6fd22f563c43a0820bdb8
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2024-07-01 09:30:35 +0200

    Update python version

--- setup.py
+++ setup.py
@@ -35,11 +35,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[
         "numba",
         "numpy",
